5c501186369273c9433749077b064f40126343e6
Michal <malpa.w.klapkach@gmail.com>
2024-01-10 13:11:24 +0100

    Add CharactersManager (with spawning characters on start)

---
+++ Assets/Scripts/CharactersManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SelectionSystem
+{
+    public class CharactersManager : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField]
+        private int initialCharactersCount = 3;
+        [SerializeField]
+        private CharactersSpawner spawner;
+
+        [Header("States")]
+        [SerializeField]
+        private List<Character> characters;
+
+        private void Start()
+        {
+            for (int i = 0; i < initialCharactersCount; i++)
+            {
+                CreateNewCharacter();
+            }
+        }
+
+        private void CreateNewCharacter()
+        {
+            var character = spawner.SpawnCharacter();
+            Vector2 flatPosition = 5 * Random.insideUnitCircle;
+            character.transform.position = new Vector3(flatPosition.x, 0, flatPosition.y);
+            characters.Add(character);
+        }
+    }
+}
