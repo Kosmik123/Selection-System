a852f524a0a956d4f30cc4f830fc3db1bdb704b8
Michal <malpa.w.klapkach@gmail.com>
2024-01-10 13:23:17 +0100

    Add CharacterButton class and create button prefab

---
+++ Assets/Scripts/UI/CharacterButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SelectionSystem.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class CharacterButton : MonoBehaviour
+    {
+        public event System.Action<Character> OnCharacterChosen;
+
+        private Button _button;
+        public Button Button
+        {
+            get
+            {
+                if ( _button == null )
+                    _button = GetComponent<Button>();
+                return _button;
+            }
+        }
+
+        [SerializeField]
+        private Character character;
+
+        private void OnEnable()
+        {
+            Button.onClick.AddListener(Button_OnClick);       
+        }
+
+        private void Button_OnClick()
+        {
+            OnCharacterChosen?.Invoke(character);
+        }
+
+        private void OnDisable()
+        {
+            Button.onClick.RemoveListener(Button_OnClick);       
+        }
+    }
+}
