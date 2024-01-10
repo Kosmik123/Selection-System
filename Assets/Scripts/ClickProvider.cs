f82d2b87cd14aa9e36a67630b26eabb08f43334c
Michal <malpa.w.klapkach@gmail.com>
2024-01-10 14:37:55 +0100

    Add ClickProvider with implementation

---
+++ Assets/Scripts/ClickProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace SelectionSystem
+{
+    public abstract class ClickProvider : MonoBehaviour
+    {
+        public abstract event Action OnClicked;
+    }
+}
---
+++ Assets/Scripts/InputManagerClickProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace SelectionSystem
+{
+    public class InputManagerClickProvider : ClickProvider
+    {
+        public override event Action OnClicked;
+
+        [SerializeField]
+        private MouseButton mouseButton;
+
+        private bool isPressed;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown((int)mouseButton))
+            {
+                isPressed = true;
+            }
+            else if (isPressed &&  Input.GetMouseButtonUp((int)mouseButton)) 
+            {
+                isPressed = false;
+                OnClicked?.Invoke();
+            }
+        }
+    }
+}
