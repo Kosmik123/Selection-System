93314a52be8354df1d5337563c3527c342c199a8
Michal <malpa.w.klapkach@gmail.com>
2024-01-10 12:52:45 +0100

    Add CharacterSettings class
    Used to specify possible property values of Chararacters

---
+++ Assets/Scripts/CharacterSettings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SelectionSystem
+{
+    [CreateAssetMenu]
+    public class CharacterSettings : ScriptableObject
+    {
+        [SerializeField]
+        private float minMoveSpeed;
+        [SerializeField]
+        private float maxMoveSpeed;
+        [SerializeField]
+        private float minRotationSpeed;
+        [SerializeField]
+        private float maxRotationSpeed;
+        [SerializeField]
+        private float minEndurance;
+        [SerializeField]
+        private float maxEndurance;
+
+        public float GetMoveSpeed() => GetRandom(minMoveSpeed, maxMoveSpeed);   
+        public float GetRotationSpeed() => GetRandom(minRotationSpeed, maxRotationSpeed);
+        public float GetEndurance() => GetRandom(minEndurance, maxEndurance);
+
+        private float GetRandom(float min, float max)
+        {
+            return Random.Range(min, max);
+        }
+    }
+}
