bd3cfba929d34f8350fd9105ffac5af2c71ee597
Michal <malpa.w.klapkach@gmail.com>
2024-01-16 10:25:18 +0100

    Add and start using ClickProvider based on event system
    Prevents from click on ground while interacting with UI

---
+++ Assets/Scripts/WalkableSurfaceClickProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine.EventSystems;
+
+namespace SelectionSystem
+{
+    public class WalkableSurfaceClickProvider : ClickProvider, IPointerClickHandler
+    {
+        public override event Action OnClicked; 
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnClicked?.Invoke();
+        }
+    }
+}
