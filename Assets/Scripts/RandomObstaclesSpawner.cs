fa33ae0fddeb0b304b7a04f3b6a50b42f8d0bdbe
Michal <malpa.w.klapkach@gmail.com>
2024-01-16 14:03:02 +0100

    Create LevelManager
    For better control over flow of game

--- Assets/Scripts/RandomObstaclesSpawner.cs
+++ Assets/Scripts/RandomObstaclesSpawner.cs
@@ -1,37 +1,40 @@
 using Unity.AI.Navigation;
 using UnityEngine;
 
-public class RandomObstaclesSpawner : MonoBehaviour
+namespace SelectionSystem
 {
-    [SerializeField]
-    private int obstaclesCount = 10;
-    [SerializeField]
-    private NavMeshSurface navMeshSurface;
-
-    private void Awake()
+    public class RandomObstaclesSpawner : MonoBehaviour
     {
-        for (int i = 0; i < obstaclesCount; i++)
+        [SerializeField]
+        private int obstaclesCount = 10;
+        [SerializeField]
+        private NavMeshSurface navMeshSurface;
+
+        public void CreateRandomEnvironment()
         {
-            float x = GetRandomValueOnTestPlane();
-            float z = GetRandomValueOnTestPlane();
-            float xScale = Random.Range(1f, 5f);
-            float zScale = Random.Range(1f, 5f);
+            for (int i = 0; i < obstaclesCount; i++)
+            {
+                float x = GetRandomValueOnTestPlane();
+                float z = GetRandomValueOnTestPlane();
+                float xScale = Random.Range(1f, 5f);
+                float zScale = Random.Range(1f, 5f);
 
-            var obstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            obstacle.transform.parent = transform;
-            obstacle.transform.SetPositionAndRotation(new Vector3(x, 0.5f, z), Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up));
-            obstacle.transform.localScale = new Vector3(xScale, 1, zScale);
+                var obstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obstacle.transform.parent = transform;
+                obstacle.transform.SetPositionAndRotation(new Vector3(x, 0.5f, z), Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up));
+                obstacle.transform.localScale = new Vector3(xScale, 1, zScale);
+            }
+            navMeshSurface.BuildNavMesh();
         }
-        navMeshSurface.BuildNavMesh();
-    }
 
-    private static float GetRandomValueOnTestPlane()
-    {
-        return Random.Range(2f, 50f) * RandomSign();
-    }
+        private static float GetRandomValueOnTestPlane()
+        {
+            return Random.Range(2f, 50f) * RandomSign();
+        }
 
-    private static int RandomSign()
-    {
-        return 2 * Random.Range(0, 2) - 1;
+        private static int RandomSign()
+        {
+            return 2 * Random.Range(0, 2) - 1;
+        }
     }
 }
